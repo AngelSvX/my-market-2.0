14056b8f3c4e786227c7619ce21012e329f661e3
AngelSvX <lsangelenma21@gmail.com>
2025-12-09 12:30:18 -0500

    config for production - railway

--- server/src/settings/db.js
+++ server/src/settings/db.js
@@ -4,10 +4,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const myMarketDB = mysql2.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  database: process.env.MYSQLDATABASE,
+  password: process.env.MYSQLPASSWORD,
+  port: process.env.MYSQLPORT
 })
 
 export const testConnection = async () => {
